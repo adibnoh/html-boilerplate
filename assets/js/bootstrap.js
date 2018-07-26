063547f461203e33f4fdaa2c01efafc48b27e7ce
adib-pc <bromatrik@gmail.com>
2018-07-26 21:35:16 +0800

    install and include favourite js plugin

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,1 +1,9 @@
 window.$ = window.jQuery = require('jquery');
+
+window.sortable = require('sortablejs');
+
+window.slug = require('slugify');
+
+window.datepicker = require('flatpickr');
+
+window.moment = require('fecha');
